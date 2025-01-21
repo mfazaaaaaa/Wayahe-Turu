250d51c0763f75141dde6c47c8f0dc974a35bfe5
mfazaaaaaa <muhammadfazaaisi@gmail.com>
2025-01-21 12:59:57 +0700

    Scene story, win, lose

--- Assets/Faza/Script/GameController.cs
+++ Assets/Faza/Script/GameController.cs
@@ -7,6 +7,9 @@ public class GameController : MonoBehaviour
     public StoryScene currentScene;
     public BottomBarController bottomBar;
     public BackgroundController backgroundController;
+    public StoryScene storyScene4;
+    public StoryScene winScene;
+    public StoryScene loseScene;
 
     void Start()
     {
@@ -22,7 +25,29 @@ public class GameController : MonoBehaviour
             {
                 if (bottomBar.IsLastSentence())
                 {
+                    // Jika berada di scene 4 dan percakapan selesai, pindah ke scene Gameplay
+                    if (currentScene == storyScene4)
+                    {
+                        SceneManager.LoadScene("Gameplay");  // Gantilah dengan nama scene gameplay Anda
+                        return;
+                    }
+
+                    // Jika berada di scene Win dan percakapan selesai, pindah ke MainMenu
+                    if (currentScene == winScene)
+                    {
+                        SceneManager.LoadScene("MainMenu");  // Pindah ke scene MainMenu
+                        return;
+                    }
+
+                    // Jika berada di scene Lose dan percakapan selesai, pindah ke MainMenu
+                    if (currentScene == loseScene)
+                    {
+                        SceneManager.LoadScene("MainMenu");  // Pindah ke scene MainMenu
+                        return;
+                    }
+
                     if (currentScene.nextScene == null) return;
+
                     currentScene = currentScene.nextScene;
                     bottomBar.PlayScene(currentScene);
                     backgroundController.SwitchImage(currentScene.background);
